736db191510e317d837ed7aea7eb2cf5d6422677
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-08-12 09:34:57 -0300

    Initial commit

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletSpawn = null;
+    public float reloadTime;
+    public float inacuracy;
+    float currReloadTime;
+    bool canShoot = true;
+    void Start()
+    {
+        currReloadTime = reloadTime;
+    }
+    void Update()
+    {
+        if(currReloadTime > 0)
+        {
+            currReloadTime -= Time.deltaTime;
+        }
+        if(Input.GetMouseButton(0) && currReloadTime <= 0)
+        {
+            var b = Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
+            //b.transform.eulerAngles += new Vector3(Random.Range(-inacuracy, inacuracy), Random.Range(-inacuracy, inacuracy), Random.Range(-inacuracy, inacuracy));
+            //esta linea esta por si quieren hacer que la bala tenga cierta impresicion
+            //en su salida. Solo la roto en x y z al azar
+            currReloadTime = reloadTime;
+        }
+    }
+}
